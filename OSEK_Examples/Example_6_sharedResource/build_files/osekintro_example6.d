65efdb550a6970cceb56f094164742a2fd53c68a
Shreef Mohamed <shreef.m303@gmail.com>
2020-10-03 20:42:22 +0200

    Example 6 & Example 7 have been Implemented and Tested

---
+++ OSEK_Examples/Example_6_sharedResource/build_files/osekintro_example6.d
@@ -0,0 +1,6 @@
+.\build_files\osekintro_example6.o: OsekIntro_Example6.c
+.\build_files\osekintro_example6.o: ..\..\OsekLib\os.h
+.\build_files\osekintro_example6.o: .\RTE\_Target_1\../../OsCfg.h
+.\build_files\osekintro_example6.o: ..\..\OsekLib\OsTypes.h
+.\build_files\osekintro_example6.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\build_files\osekintro_example6.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ OSEK_Examples/Example_7_ResourcesManagement/build_files/osekintro_example7.d
@@ -0,0 +1,6 @@
+.\build_files\osekintro_example7.o: OsekIntro_Example7.c
+.\build_files\osekintro_example7.o: ..\..\OsekLib\os.h
+.\build_files\osekintro_example7.o: .\RTE\_Target_1\../../OsCfg.h
+.\build_files\osekintro_example7.o: ..\..\OsekLib\OsTypes.h
+.\build_files\osekintro_example7.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\build_files\osekintro_example7.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
